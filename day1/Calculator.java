0e64743aed0bc333b3aa4ec07c7bf8bf8c22dd18
Arun Kumar <java.kumar.arun@gmail.com>
2025-06-28 10:37:59 +0530

    Repository created at 28Jun25

---
+++ day1/Calculator.java
@@ -0,0 +1,14 @@
+package day1;
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 5;
+
+        // addition
+        int sum = num1 + num2;
+        System.out.println("Addition of "+num1+" and "+num2+" is "+sum);
+        System.out.println("Addition of "+num1+" and "+num2+" is "+(num1+num2));
+
+        // divide, subtract, multiply, greater number
+    }
+}
---
+++ day1/Demo.java
@@ -0,0 +1,22 @@
+package day1;
+public class Demo {
+
+    public static void main(String[] args){
+        // Book details
+        String title = "Harry Porter";
+        short price = 325; 
+        // int a = 5;
+        String author = "Ramesh";
+
+        System.out.print("Title: "+title);
+        System.out.print(", Price: "+price);
+        System.out.println(", Author: "+author);
+
+
+        System.out.print("Title: "+title+", Price: "+price+", Author: "+author);
+
+    }
+    
+}
+
+
