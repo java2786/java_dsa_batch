d9d52d9890ac021f81ab706babc548799985c9e3
Arun Kumar <java.kumar.arun@gmail.com>
2025-11-14 16:41:13 +0530

    14nov

---
+++ dsa_stack/L20.java
@@ -0,0 +1,35 @@
+public class L20 {
+    
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        // ( -> ) -> { -> ( -> [ -> ( -> ) -> ( -> ) -> ] -> ) -> } -> 
+        int len = s.length();
+        if(len%2!=0){
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<len;i++){
+            char c = s.charAt(i);
+            if( c == '[' || c == '{' || c == '('){
+                stack.push(c);
+            } else {
+                // c -> ),},]
+                if(stack.size()==0){
+                    return false;
+                }
+                char lc = stack.pop();
+                if (
+                    (lc == '(' && c == ')') || 
+                    (lc == '{' && c == '}') || 
+                    (lc == '[' && c == ']')){}
+                    else {
+                    return false;
+                }
+            }
+        }
+        return stack.size()==0;
+    }
+}
